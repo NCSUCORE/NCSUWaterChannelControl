ee345bd32670d23b6a42b67965b29792d7db03d8
mcobb <mcobb@VermillionLab1.mae.ncsu.edu>
2019-02-13 18:36:25 -0500

    Modified image processing library to calculate dot centroid locations using rotation matrices.  - MC

--- compositions/imageProcessing/imageProcessingInit.m
+++ compositions/imageProcessing/imageProcessingInit.m
@@ -1,7 +1,16 @@
 %% Function to initialize the image processing block
-% Basically all this does is set the underlying parameters on the DotFinder
-% block to match the parameters from this mask
 
 set_param([gcb '/DotFinder'],'DotOrientation',get_param(gcb,'DotOrientation'))
 set_param([gcb '/DotFinder'],'ROIPositionHoldEnable',get_param(gcb,'ROIPositionHoldEnable'))
 set_param([gcb '/DotFinder'],'OverlayOutput',get_param(gcb,'OverlayOutput'))
+
+% Calculate the rotation matrix that takes a vector in the camera frame and
+% produces the vector in the ground frame.  See rotation matrix.nb in the
+% documentation folder for details.
+roll  = camEulerAngles_deg(3)*pi/180; % Yaw
+pitch = camEulerAngles_deg(2)*pi/180; % Pitch
+yaw   = camEulerAngles_deg(1)*pi/180; % Roll
+cam2GroundRotationMatrix = (calculateRotationMatrix(roll,pitch,yaw))';
+
+
+
