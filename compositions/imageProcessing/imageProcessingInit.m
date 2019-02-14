07adfa977341e1be0b9ce45127a230df22d3320a
mcobb <mcobb@VermillionLab1.mae.ncsu.edu>
2019-02-13 20:47:34 -0500

    Added matrix math/rotation to vector component calculations.  -MC

--- compositions/imageProcessing/imageProcessingInit.m
+++ compositions/imageProcessing/imageProcessingInit.m
@@ -7,9 +7,26 @@ set_param([gcb '/DotFinder'],'OverlayOutput',get_param(gcb,'OverlayOutput'))
 % Calculate the rotation matrix that takes a vector in the camera frame and
 % produces the vector in the ground frame.  See rotation matrix.nb in the
 % documentation folder for details.
-roll  = camEulerAngles_deg(3)*pi/180; % Yaw
-pitch = camEulerAngles_deg(2)*pi/180; % Pitch
-yaw   = camEulerAngles_deg(1)*pi/180; % Roll
+
+switch lower(cameraSelection)
+    case 'bottoma'
+        roll  = 0;
+        pitch = 180*pi/180;
+        yaw   = 180*pi/180;
+    case 'bottomb'
+        roll  = 0;
+        pitch = 180*pi/180;
+        yaw   = 180*pi/180;
+    case 'side'
+        roll  = 0;
+        pitch = 90*pi/180;
+        yaw   = -90*pi/180;
+    case 'slant'
+        roll  = 0;
+        pitch = 135*pi/180;
+        yaw   = 180*pi/180;
+end
+
 cam2GroundRotationMatrix = (calculateRotationMatrix(roll,pitch,yaw))';
 
 
