a0e8817993d2ee1bf35c3c38af338952f8157bb4
MAE-NCSUCORE <MAE-NCSUCORE@VermillionLab4.mae.ncsu.edu>
2019-02-19 18:11:47 -0500

    Modified initialization script to open models, build models, and connect to targets.

--- initializeSystem.m
+++ initializeSystem.m
@@ -25,23 +25,47 @@ delete(t);
 
 if strcmpi(answer,'y')
     fprintf('\nOpening all models\n')
+    % Window positions determined from using get_param(gcs,'location')
+    windowPositions = [ -6     0   775   418;...
+                        762           0        1543         418;...
+                        -6   412   775   830;...
+                        762         412        1543         830];
     % Open all the individual models
     for ii = 2:5
         open_system(fullfile(baseDir,'models',sprintf('model%d',ii),sprintf('model%d_cm.slx',ii)))
         % Add code here to reposition/resize windows to tile them
+        set_param(gcs,'location',windowPositions(ii-1,:));
+        % Zoom to fit
+        set_param(gcs, 'ZoomFactor','FitSystem')
+        
     end
     
     % Build and connect
     fprintf('\nBuilding all models\n')
-    % Put code to build models and connect here.
-    fprintf('\nConnecting to targets\n')
-    % Check for target first probably ought to use some kind of try/catch
-    % Add code to open GUI once we build it.
+    for ii = 2:5
+        % Build models:
+        try
+            fprintf('\nBuilding model%d_cm.slx\n',ii)
+            eval(sprintf('model%d_cm([], [], [], ''compile'')',ii));
+            % Sometimes the above line results in a bug that locks the
+            % models from editing etc.  To break out of this use: model2_cm([], [], [], 'term')
+            % You may need to run that multiple times to get it to work.
+        catch errMsg
+            rethrow(errMsg)
+        end
+        % Connect to targets:
+        try
+            fprintf('\nConnecting to target machine %d\n',ii)
+            set_param(sprintf('model%d_cm',ii),'SimulationCommand','connect');
+        catch errMsg
+            rethrow(errMsg)
+        end
+    end
 end
 
 fprintf('\nDone\n')
 
-clearvars t ii baseDir answer ans
+clearvars t ii baseDir answer ans windowPositions_px errMsg
 
 function pressEnter(HObj, event)
 % Code copied from: https://www.mathworks.com/matlabcentral/answers/119067-how-can-i-set-a-time-for-input-waiting
