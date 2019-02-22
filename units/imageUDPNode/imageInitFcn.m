bfff3b08a6cd8b45af5fab7d06fe3aafd765b97f
MAE-NCSUCORE <MAE-NCSUCORE@VermillionLab4.mae.ncsu.edu>
2019-02-22 17:00:22 -0500

    Final Validation complete - Jack

--- units/imageUDPNode/imageInitFcn.m
+++ units/imageUDPNode/imageInitFcn.m
@@ -1,12 +1,12 @@
-function imageInitFcn(blockPath,systemPath)
+function imageInitFcn(blockPath)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-open_system(systemPath,'force') %'force' option looks under block mask
-recv_path = blockPath; 
-config_pth = [systemPath '/IEEE 1588 Real-Time UDP']; 
+open_system(blockPath,'force') %'force' option looks under block mask
+recv_path = [blockPath '/UDP Receive']; 
+config_pth = [blockPath '/IEEE 1588 Real-Time UDP']; 
 
-correctIP = get_param(config_pth,'IpAddress');
+correctIP = get_param(config_pth,'Receive1');
 set_param(recv_path,'ipAddress',correctIP); 
 close_system(systemPath);
 
