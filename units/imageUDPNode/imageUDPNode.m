190777179c59bc98abdd1c6250f500519988d24b
MAE-NCSUCORE <MAE-NCSUCORE@VermillionLab4.mae.ncsu.edu>
2019-02-26 13:20:32 -0500

    Fixing imageUDPNode - Jack

--- units/imageUDPNode/imageUDPNode.m
+++ units/imageUDPNode/imageUDPNode.m
@@ -1,26 +1,27 @@
+block1 = find_system(gcb,...
+    'LookUnderMasks','on',...
+    'FollowLinks','on','Name','pixelCompA');
+
 block2 = find_system(gcb,...
     'LookUnderMasks','on',...
-    'FollowLinks','on','Name','In2');
+    'FollowLinks','on','Name','pixelCompB');
 
-block3 = find_system(gcb,...
-    'LookUnderMasks','on',...
-    'FollowLinks','on','Name','In3');
 
 packingPorts = get_param([gcb '/Byte Packing'],'PortConnectivity');
 
 switch length(packingPorts)
-    case 2
-        if (~isempty(block2) || ~isempty(block3))
-            delete_block(block2)
+    case 1
+        if (~isempty(block1) || ~isempty(block3))
+            delete_block(block1)
             delete_block(block3)
             
             posX = packingPorts(1).Position(1) - 85;
             posY = packingPorts(1).Position(2) - 10;
             pos = [posX, posY, posX+35, posY+15];
-            set_param([gcb '/In1'],'position',pos);
+            set_param([gcb  '/In1'],'position',pos);
         end
-    case 3
-        if isempty(block2)
+    case 2
+        if isempty(block1)
             add_block('Simulink/Ports & Subsystems/In1',[gcb '/In2'])
             posX = packingPorts(2).Position(1) - 85;
             posY = packingPorts(2).Position(2) - 10;
@@ -31,23 +32,5 @@ switch length(packingPorts)
             posY = packingPorts(1).Position(2) - 10;
             pos = [posX, posY, posX+35, posY+15];
             set_param([gcb '/In1'],'position',pos);
-        end
-    case 4
-        if isempty(block3)
-            add_block('Simulink/Ports & Subsystems/In1',[gcb '/In3'])
-            posX = packingPorts(2).Position(1) - 85;
-            posY = packingPorts(2).Position(2) - 10;
-            pos = [posX, posY, posX+35, posY+15];
-            set_param([gcb '/In2'],'position',pos);
-            
-            posX = packingPorts(1).Position(1) - 85;
-            posY = packingPorts(1).Position(2) - 10;
-            pos = [posX, posY, posX+35, posY+15];
-            set_param([gcb '/In1'],'position',pos);
-            
-            posX = packingPorts(3).Position(1) - 85;
-            posY = packingPorts(3).Position(2) - 10;
-            pos = [posX, posY, posX+35, posY+15];
-            set_param([gcb '/In3'],'position',pos);
         end
 end 
