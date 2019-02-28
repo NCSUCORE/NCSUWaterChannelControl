5696f7b02c8850137aeb7859c0916b8a428ea14f
MAE-NCSUCORE <MAE-NCSUCORE@VermillionLab4.mae.ncsu.edu>
2019-02-28 13:19:21 -0500

    Editing GUI - Jack

--- functions/findLimits.m
+++ functions/findLimits.m
@@ -2,5 +2,12 @@ function limit = findLimits(parameterName)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+varName = sprintf('params.(''%s'')', parameterName);
+var = evalin('base',varName);
+max = var.Max;
+min = var.Min;
+
+limit = [max min];
+
 end
 
