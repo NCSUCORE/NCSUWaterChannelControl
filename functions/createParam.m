582148c769c3a968c02f2ac38a490d3042de4e3e
MAE-NCSUCORE <MAE-NCSUCORE@VermillionLab4.mae.ncsu.edu>
2019-03-04 17:39:27 -0500

    Entered all GUI callbacks - Jack

--- functions/createParam.m
+++ functions/createParam.m
@@ -3,18 +3,20 @@ function createParam(name,value,min,max,description,units)
 % pre-pend the units to the description
 description = ['[' units '] ' description];
 
-if (isscalar(value) || ischar(value))
+if isscalar(value)
     evalin('base',sprintf('params.%s = Simulink.Parameter(%s);',name,num2str(value)));
     evalin('base',sprintf('params.%s.Max = %s;',name,num2str(max)));
     evalin('base',sprintf('params.%s.Min = %s;',name,num2str(min)));
     evalin('base',sprintf('params.%s.Dimensions = [1 1];',name));
     evalin('base',sprintf('params.%s.Description = ''%s'';',name,description));
-% elseif ischar(value)
-%     evalin('base',sprintf('params.%s = Simulink.Parameter(%s);',name,value)));
-%     evalin('base',sprintf('params.%s.Max = %s;',name,num2str(max)));
-%     evalin('base',sprintf('params.%s.Min = %s;',name,num2str(min)));
-%     evalin('base',sprintf('params.%s.Dimensions = [1 1];',name));
-%     evalin('base',sprintf('params.%s.Description = ''%s'';',name,description));
+elseif ischar(value)
+    str = strrep(value,'.',' ');
+    evalin('base',sprintf('params.%s = Simulink.Parameter([%s]);',name,str));
+    evalin('base',sprintf('params.%s.Max = %s;',name,num2str(max)));
+    evalin('base',sprintf('params.%s.Min = %s;',name,num2str(min)));
+    % Do not use str2double, does not work in this context.
+    evalin('base',sprintf('params.%s.Dimensions = [%s];',name,num2str(size(str2num(str)))));
+    evalin('base',sprintf('params.%s.Description = ''%s'';',name,description));
 else
     evalin('base',sprintf('params.%s = Simulink.Parameter([%s]);',name,num2str(value)));
     % For vector quantities, specify min/max values scalars, min and max
