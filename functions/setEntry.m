dcfe090c09577eabf5ad1a9caa7b2e40b08af005
MAE-NCSUCORE <MAE-NCSUCORE@VermillionLab4.mae.ncsu.edu>
2019-03-25 13:50:03 -0400

    Added set_params to GUI - Jack

--- functions/setEntry.m
+++ functions/setEntry.m
@@ -6,27 +6,41 @@ function variable = setEntry(parameterName,value,varargin)
 
 if (variable.Dimensions(2) > 1)
     variable.Value(varargin{1}) = value;
-else
-    variable.Value = value;
-end
-
-setValue(entry,variable);
-
-if (length(varargin) > 1)
-    if (strcmp(varargin{1},'model2_cm') || strcmp(varargin{2},'model2_cm'))
-        set_param('model2_cm','SimulationCommand','Update');
-    end
+    
     if (length(varargin) > 2)
-        if (variable.Dimensions(2) == 1)
-            str = sprintf('%d',variable.Value);
-        elseif (variable.Dimensions(2) == 2)
+        if (variable.Dimensions(2) == 2)
             str = sprintf('[%d %d]',variable.Value);
-        else
+        elseif (variable.Dimensions(2) == 3)
             str = sprintf('[%d %d %d]',variable.Value);
         end   
         set_param(varargin{2},varargin{3},str);
     end
+else
+    variable.Value = value;
+    
+    if (length(varargin) == 2)
+        str = sprintf('%d',variable.Value);
+        set_param(varargin{1},varargin{2},str);
+    end
 end
+
+setValue(entry,variable);
+
+% if (length(varargin) > 1)
+%     if (strcmp(varargin{1},'model2_cm') || strcmp(varargin{2},'model2_cm'))
+%         set_param('model2_cm','SimulationCommand','Update');
+%     end
+%     if (length(varargin) > 2)
+%         if (variable.Dimensions(2) == 1)
+%             str = sprintf('%d',variable.Value);
+%         elseif (variable.Dimensions(2) == 2)
+%             str = sprintf('[%d %d]',variable.Value);
+%         else
+%             str = sprintf('[%d %d %d]',variable.Value);
+%         end   
+%         set_param(varargin{2},varargin{3},str);
+%     end
+% end
 
 end
 
