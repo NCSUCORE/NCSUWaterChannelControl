2d68eb508037eca4eafca05c093f544ea9c25038
mcobb <mcobb@VermillionLab1.mae.ncsu.edu>
2019-04-02 18:10:52 -0400

    Added another case to constant setpoints test script.  Validated fault response of controller.  Modified test harness accordingly.

--- compositions/controllers/constantSetpoints/constantSetpoints_ts.m
+++ compositions/controllers/constantSetpoints/constantSetpoints_ts.m
@@ -1,8 +1,13 @@
 close all
 clear
 clc
+Tf = 10;
+Ts = 1e-3;
 
-test = 'roll';
+test = 'fault';
+
+time = 0:Ts:Tf;
+faultStatus = false(size(time));
 
 switch test
     case 'pitch'
@@ -10,17 +15,24 @@ switch test
         pitch_deg = 1;
         yaw_deg   = 0;
         CoMPositionVec_cm = [0 0 39];
-        faultStatus = false;
         
     case 'roll'
         roll_deg  = 1;
         pitch_deg = 0;
         yaw_deg   = 0;
         CoMPositionVec_cm = [0 0 39];
-        faultStatus = false;
-        
+                
+    case 'fault'
+        roll_deg  = 1;
+        pitch_deg = 0;
+        yaw_deg   = 0;
+        CoMPositionVec_cm = [0 0 40];
+        faultStatus(and(time>4,time<6)) = true;
+         
     otherwise
 end
+
+faultStatus = timeseries(faultStatus,time);
 
 sim('constantSetpoints_th')
 
@@ -31,6 +43,3 @@ subplot(3,1,2)
 mtr2.plot
 subplot(3,1,3)
 mtr3.plot
-
-figure
-simout.plot
