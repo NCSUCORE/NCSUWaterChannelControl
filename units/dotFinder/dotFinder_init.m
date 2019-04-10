c8f4953d2d6cb59910d0b0fc30ebd3c55652964e
mae-ncsucore <mae-ncsucore@VermillionLab4.mae.ncsu.edu>
2019-04-10 19:13:19 -0400

    Code Cleanup - Jack

--- units/dotFinder/dotFinder_init.m
+++ units/dotFinder/dotFinder_init.m
@@ -1,27 +1,34 @@
-% Calculate the ROI half width and half height
 
+% Calculate the ROI half width and half height
 ROIHalfDimVec_px = [floor(ROIDimVec_px(1)/2)...
     floor(ROIDimVec_px(2)/2)];
 
+% Set ROIHalfDimVec_px parameter in block mask as determined by above
+% equation
 set_param(gcb,'ROIHalfDimVec_px',[ '[' num2str(ROIHalfDimVec_px(1)) ' ' num2str(ROIHalfDimVec_px(2)) ']' ])
+
 % Set up the matrix that determines how to split the ROI
-% before we do the centroid step
+% before we calculate the dot centroid
 
 % Matrix has form
-% [startPoint, widthORHeight
+% [startPoint, widthORHeight]
 if strcmpi(get_param(gcb,'DotOrientation'),'Horizontal')
+    
     % If the ROI is horizontal, then split down the middle
     % Set the limits on the rows
     ROISplitDims_px(1,:) = [1 ROIDimVec_px(1)];
     ROISplitDims_px(3,:) = [1 ROIDimVec_px(1)];
+    
     % Set the limits on the columns
     ROISplitDims_px(2,:) = [1 ROIHalfDimVec_px(2)];
     ROISplitDims_px(4,:) = [ROIHalfDimVec_px(2)+1 ROIHalfDimVec_px(2)-1];
 else
+    
     % If the ROI is vertical, then split across the middle
     % Set the limits on the rows
     ROISplitDims_px(1,:) = [1 ROIHalfDimVec_px(1)];
     ROISplitDims_px(3,:) = [ROIHalfDimVec_px(1)+1 ROIHalfDimVec_px(1)-1];
+    
     % Set the limits on the columns
     ROISplitDims_px(2,:) = [1 ROIDimVec_px(2)];
     ROISplitDims_px(4,:) = [1 ROIDimVec_px(2)];
--- units/findPositions/findPosition.m
+++ units/findPositions/findPosition.m
@@ -1,26 +1,61 @@
-function [x1,x2,y1,y2,z] = ...
-    findPosition(rSideGF,rBottomAGF,rBottomBGF,R,rSideBF,rBottomABF,rBottomBBF)
-%#codegen
+function [side2SideDist_cm,bot2BotADist_cm,...
+    bot2BotBDist_cm,slant2BotBDist_cm,CoMPositionVec_cm] = ...
+    findPosition(sideDotImageCoords,bottomADotImageCoords,...
+    bottomBDotImageCoords,...
+    body2GroundRotationMatrix,lastCoMPositionVec_cm,...
+    sideDotPositionVec_cm,bottomDotAPositionVec_cm,...
+    bottomDotBPositionVec_cm,sideCameraPositionVec_cm,...
+    bottomCameraPositionVec_cm,slantCameraPositionVec_cm,...
+    alphaBar,alpha0,gamma,imageDims,cameraFOVAngle_deg)
 
-% Find x from side camera
-% Find y from bottom A dots
-x1 = rBottomAGF(1);
-y1 = rBottomAGF(2);
+%FINDPOSITION Summary of this function goes here
+%   Detailed explanation goes here
 
-x1 = x1 - (R(1,1)*rSideBF(1)    + R(1,2)*rSideBF(2)    + R(1,3)*rSideBF(3));
-y1 = y1 - (R(2,1)*rBottomABF(1) + R(2,2)*rBottomABF(2) + R(2,3)*rBottomABF(3));
+% Vectors that point from the CoM to the dots represented in the ground frame
+side2CoMInGFVec_cm   = body2GroundRotationMatrix*sideDotPositionVec_cm;
+botA2CoMInGFVec_cm   = body2GroundRotationMatrix*bottomDotAPositionVec_cm;
+botB2ComInGFVec_cm   = body2GroundRotationMatrix*bottomDotBPositionVec_cm;
 
-% Find x from bottom A dots
-% Find y from bottom B dots
-% Note: these values are not used in the model, just logged.
-x2 = rSideGF(1);
-y2 = rBottomBGF(2);
+% Calculate distance from camera to specific dots
+side2SideDist_cm = norm(sideCameraPositionVec_cm - (lastCoMPositionVec_cm + side2CoMInGFVec_cm));
+bot2BotADist_cm  = norm(bottomCameraPositionVec_cm - (lastCoMPositionVec_cm + botA2CoMInGFVec_cm));
+bot2BotBDist_cm  = norm(bottomCameraPositionVec_cm - (lastCoMPositionVec_cm + botB2ComInGFVec_cm));
+slant2BotBDist_cm = norm(slantCameraPositionVec_cm - (lastCoMPositionVec_cm + botB2ComInGFVec_cm));
 
-x2 = x2 - (R(1,1)*rBottomABF(1) + R(1,2)*rBottomABF(2) + R(1,3)*rBottomABF(3));
-y2 = y2 - (R(2,1)*rBottomBBF(1) + R(2,2)*rBottomBBF(2) + R(2,3)*rBottomBBF(3));
+% Find position of side dots in camera frame from side camera
+sidePosVecCamFrame_cm = zeros(2,1);
+sidePosVecCamFrame_cm(1) = (2*side2SideDist_cm/imageDims(1))*(sideDotImageCoords(1) - imageDims(1)/2)*tand(cameraFOVAngle_deg(1));
+sidePosVecCamFrame_cm(2) = (2*side2SideDist_cm/imageDims(2))*(sideDotImageCoords(2) - imageDims(2)/2)*tand(cameraFOVAngle_deg(2));
 
-% Find z from side camera (only way possible).
-z = rSideGF(3);
-z = z - (R(3,1)*rSideBF(1)    + R(3,2)*rSideBF(2)    + R(3,3)*rSideBF(3));
+% Find position of bottom dots A in camera frame from bottom camera
+botAPosVecCamFrame_cm = zeros(2,1);
+botAPosVecCamFrame_cm(1) = (2*bot2BotADist_cm/imageDims(1))*(bottomADotImageCoords(1) - imageDims(1)/2)*tand(cameraFOVAngle_deg(1));
+botAPosVecCamFrame_cm(2) = (2*bot2BotADist_cm/imageDims(2))*(bottomADotImageCoords(2) - imageDims(2)/2)*tand(cameraFOVAngle_deg(2));
 
+% Find position of bottom dots B in camera frame from bottom camera
+botBPosVecCamFrame_cm = zeros(2,1);
+botBPosVecCamFrame_cm(1) = (2*bot2BotBDist_cm/imageDims(1))*(bottomBDotImageCoords(1) - imageDims(1)/2)*tand(cameraFOVAngle_deg(1));
+botBPosVecCamFrame_cm(2) = (2*bot2BotBDist_cm/imageDims(2))*(bottomBDotImageCoords(2) - imageDims(2)/2)*tand(cameraFOVAngle_deg(2));
+
+
+Delta = [sidePosVecCamFrame_cm(1);... % a
+    sidePosVecCamFrame_cm(2);... % b
+    botAPosVecCamFrame_cm(1);... % c
+    botBPosVecCamFrame_cm(1);... % d
+    botAPosVecCamFrame_cm(2);... % e
+    botBPosVecCamFrame_cm(2)]; % f
+
+rDB = body2GroundRotationMatrix * sideDotPositionVec_cm;
+rEB = body2GroundRotationMatrix * bottomDotAPositionVec_cm;
+rFB = body2GroundRotationMatrix * bottomDotBPositionVec_cm;
+
+beta = [rDB;rDB;rEB;rFB;rEB;rFB];
+
+CoMPositionVec_cm = alphaBar*(Delta+alpha0*(gamma-beta));
+side2SideDist_cm  = norm(CoMPositionVec_cm + rDB - sideCameraPositionVec_cm);
+bot2BotADist_cm   = norm(CoMPositionVec_cm + rEB - bottomCameraPositionVec_cm);
+bot2BotBDist_cm   = norm(CoMPositionVec_cm + rFB - bottomCameraPositionVec_cm);
+slant2BotBDist_cm = norm(CoMPositionVec_cm + rFB - slantCameraPositionVec_cm);
+
+end
 
