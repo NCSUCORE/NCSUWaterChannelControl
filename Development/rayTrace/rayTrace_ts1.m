6bb260a7043641e71ff563759d3f8134a247da01
mcobb <mcobb@VermillionLab1.mae.ncsu.edu>
2019-04-26 18:27:01 -0400

    Debugged rayTrace_ul

--- Development/rayTrace/rayTrace_ts1.m
+++ Development/rayTrace/rayTrace_ts1.m
@@ -4,46 +4,58 @@ clear
 clc
 
 % Start by testing this (plot the same thing from the diagram)
-camPosVec_cm   = [-100 0 -5]';
-roll_rad   = [0  ]*pi/180;
-pitch_rad = [135]*pi/180;
-yaw_rad    = [180]*pi/180;
+camPosVec_cm      = [-100 0 -5]';
+roll_deg          = 0;
+pitch_deg         = 135;
+yaw_deg           = 180;
 glassThickness_cm = 2;
 dist2Glass_cm     = 5;
-glassPlane     = 'xy';
+glassPlane        = 1;
 indOfRefVec       = [1.000293,1.52,1.333];
+imageDims_px      = [1000 2000];
+cameraFOV_deg     = [40 20];
 
-imageDims_px = [1000 2000];
-cameraFOV_deg = [40 20];
+grnd2CamRotMat = calculateRotationMatrix(....
+    roll_deg*pi/180,pitch_deg*pi/180,yaw_deg*pi/180);
 
-for kk = 1:length(roll_rad)
-    roll_rad  = roll_rad(kk);
-    pitch = pitche_rad(kk);
-    yaw_rad   = yaw_rad(kk);
-    grnd2CamRotMat = calculateRotationMatrix(roll_rad,pitch,yaw_rad);
-    RogG = [];
-    RigG =[];
-    for ii = 1:length(gamma)
-            [rogG,rigG,uiG] = ...
-                rayTrace(camPosVec,grnd2CamRotMat,dist2Glass,...
-                gamma(ii,1),gamma(ii,2),indOfRefVec,glassPlane,glassThickness);
-            RogG(end+1,:)=rogG';
-            RigG(end+1,:)=rigG';
-            r = [camPosVec';rogG';rigG'];
-            plot3(r(:,1),r(:,2),r(:,3),'LineStyle','-')
-            hold on
-            scatter3(r(:,1),r(:,2),r(:,3))
-            quiver3(rigG(1),rigG(2),rigG(3),dist2Glass*uiG(1),dist2Glass*uiG(2),dist2Glass*uiG(3),'Color',colors{kk},'LineStyle','-');
-    end
-    % Plot the glass inside and outside planes
-    plot3(RogG(:,1),RogG(:,2),RogG(:,3),'LineWidth',2,'Color','g','LineStyle','-')
-    plot3(RigG(:,1),RigG(:,2),RigG(:,3),'LineWidth',2,'Color','b','LineStyle','-')
-    
-    % Plot camera axes
-    scatter3(camPosVec(1),camPosVec(2),camPosVec(3))
-    quiver3(camPosVec(1),camPosVec(2),camPosVec(3),grnd2CamRotMat(1,1),grnd2CamRotMat(1,2),grnd2CamRotMat(1,3),'Color',[1 0 0])
-    quiver3(camPosVec(1),camPosVec(2),camPosVec(3),grnd2CamRotMat(2,1),grnd2CamRotMat(2,2),grnd2CamRotMat(2,3),'Color',[1 0 0])
-    quiver3(camPosVec(1),camPosVec(2),camPosVec(3),grnd2CamRotMat(3,1),grnd2CamRotMat(3,2),grnd2CamRotMat(3,3),'Color',[1 0 0])
-    
-    axis equal
+pixelRows     = [0:imageDims_px(1)/5:imageDims_px(1)]';
+pixelCols     = [0:imageDims_px(2)/5:imageDims_px(2)]';
+
+pixelCoords = [...
+    pixelRows zeros(size(pixelRows));...
+    pixelRows(end)*ones(size(pixelCols)) pixelCols;...
+    flip(pixelRows) pixelCols(end)*ones(size(pixelRows));...
+    pixelRows(1)*ones(size(pixelCols)) flip(pixelCols)];
+
+plot(pixelCoords(:,2),pixelCoords(:,1),'Color','r','LineWidth',2)
+figure
+colors ={[0 0 1],[1 0 0]};
+RogG = [];
+RigG = [];
+for ii = 1:length(pixelCoords)
+    pixelRow = pixelCoords(ii,1);
+    pixelCol = pixelCoords(ii,2);
+     
+    sim('rayTrace_th')
+    RogG(end+1,:)=rogG.data;
+    RigG(end+1,:)=rigG.data;
+
+    r = [camPosVec_cm';rogG.data;rigG.data];
+    plot3(r(:,1),r(:,2),r(:,3),'LineStyle','-')
+    hold on
+    scatter3(r(:,1),r(:,2),r(:,3))
+    quiver3(rigG.data(1),rigG.data(2),rigG.data(3),...
+        dist2Glass_cm*uiG.data(1),dist2Glass_cm*uiG.data(2),dist2Glass_cm*uiG.data(3),'LineStyle','-');
+    drawnow
 end
+% Plot the glass inside and outside planes
+plot3(RogG(:,1),RogG(:,2),RogG(:,3),'LineWidth',2,'Color','g','LineStyle','-')
+plot3(RigG(:,1),RigG(:,2),RigG(:,3),'LineWidth',2,'Color','b','LineStyle','-')
+
+% Plot camera axes
+scatter3(camPosVec_cm(1),camPosVec_cm(2),camPosVec_cm(3))
+quiver3(camPosVec_cm(1),camPosVec_cm(2),camPosVec_cm(3),grnd2CamRotMat(1,1),grnd2CamRotMat(1,2),grnd2CamRotMat(1,3),'Color',[1 0 0])
+quiver3(camPosVec_cm(1),camPosVec_cm(2),camPosVec_cm(3),grnd2CamRotMat(2,1),grnd2CamRotMat(2,2),grnd2CamRotMat(2,3),'Color',[1 0 0])
+quiver3(camPosVec_cm(1),camPosVec_cm(2),camPosVec_cm(3),grnd2CamRotMat(3,1),grnd2CamRotMat(3,2),grnd2CamRotMat(3,3),'Color',[1 0 0])
+axis equal
+
