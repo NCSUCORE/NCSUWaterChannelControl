286474e7d5d42158ee03728b9bf7739687f66576
MAE-NCSUCORE <MAE-NCSUCORE@VermillionLab4.mae.ncsu.edu>
2019-04-26 18:06:49 -0400

    Snell's Law resolveCOM position block - Jack

--- Development/rayTrace/resolveCOM.m
+++ Development/rayTrace/resolveCOM.m
@@ -1,4 +1,4 @@
-function [distanceVec] = resolveCOM(rCentroidVec,uCentroidVec,...
+function [COMPosVec] = resolveCOM(rCentroidVec,uCentroidVec,...
     body2GroundRotMat,sideDotPosVec_cm,botADotPosVec_cm,botBDotPosVec_cm)
 %RESOLVECOM Summary of this function goes here
 %   Detailed explanation goes here
@@ -9,15 +9,23 @@ botBDotGF = body2GroundRotMat*botBDotPosVec_cm;
 
 COM2DotGF = [sideDotGF botADotGF botBDotGF botBDotGF];
 
-% I = eye(3);
-% U = [I -I  0  0;
-%      I  0 -I  0;
-%      I  0  0 -I;
-%      0  I -I  0;
-%      0  I  0 -I;
-%      0  0  I -I];
+P = rCentroidVec + COM2DotGF;
 
-distanceVec = inv(-(uCentroidVec'*uCentroidVec))*uCentroidVec'*(rCentroidVec + COM2DotGF);
+I = eye(3);
+delta = [I -I  0  0;
+         I  0 -I  0;
+         I  0  0 -I;
+         0  I -I  0;
+         0  I  0 -I;
+         0  0  I -I];
+     
+alpha = uCentroidVec'*delta'*delta*uCentroidVec;
+
+distanceVec = inv(-(alpha'*alpha))*alpha'*uCentroidVec'*delta'*delta*P;
+
+COMPosVec = distanceVec*uCentroidVec + P;
+
+COMPosVec = mean(COMPosVec(:,1) + COMPosVec(:,2) + COMPosVec(:,3) + COMPosVec(:,4));
 
 end
 
