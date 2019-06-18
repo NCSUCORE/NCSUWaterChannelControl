f2ec16d4c7b0d694bbc6484b313e6a6ef7f56c3e
jackdcole@gmail.com <Panthers24cole>
2019-06-18 17:55:30 -0400

    Ray trace debugging - JC

--- compositions/find6DOFSnellMin9/find6DOFSnellMin9_ts.m
+++ compositions/find6DOFSnellMin9/find6DOFSnellMin9_ts.m
@@ -12,11 +12,11 @@ timeStart = 1000;
 timeEnd = 1050;
 index = 1;
 
-length = timeEnd - timeStart;
+len = timeEnd - timeStart;
 
-botDot1 = zeros(length,2); 
-botDot2 = zeros(length,2);
-botDot3 = zeros(length,2);
+botDot1 = zeros(len,2); 
+botDot2 = zeros(len,2);
+botDot3 = zeros(len,2);
 
 for ii = timeStart:timeEnd
     botDotCoords = tsc.botDotCoords.Data(:,:,ii);
@@ -30,3 +30,30 @@ end
 botDot1Time = timeseries(botDot1);
 botDot2Time = timeseries(botDot2);
 botDot3Time = timeseries(botDot3);
+
+%%
+
+load('C:\Users\MAE-NCSUCORE\Desktop\WaterChannelControl\output\data\data_18_Jun_2019_17_24_48.mat')
+
+timeStart = 900;
+timeEnd = 910;
+index = 1;
+
+len = timeEnd - timeStart;
+
+cam1 = zeros(len,6); 
+cam2 = zeros(len,6);
+cam3 = zeros(len,6);
+
+for ii = timeStart:timeEnd
+    cam1(index,:) = tsc.cam1DotPos.Data(:,:,ii);
+    cam2(index,:) = tsc.cam2DotPos.Data(:,:,ii);
+    cam3(index,:) = tsc.cam3DotPos.Data(:,:,ii);
+    
+    index = index + 1;
+end
+
+cam1Time = timeseries(cam1);
+cam2Time = timeseries(cam2);
+cam3Time = timeseries(cam3);
+
--- units/rayTrace/rayTrace_ts4.m
+++ units/rayTrace/rayTrace_ts4.m
@@ -3,9 +3,10 @@ clc; clear;
 format compact
 
 loadParams;
+createSnellsMinParams;
 % load('C:\Users\MAE-NCSUCORE\Desktop\WaterChannelControl\output\data\data_23_May_2019_11_20_54.mat');
 % load('C:\Users\MAE-NCSUCORE\Desktop\WaterChannelControl\output\data\data_29_May_2019_14_39_58.mat')
-load('C:\Users\MAE-NCSUCORE\Desktop\WaterChannelControl\output\data\data_12_Jun_2019_11_07_53.mat')
+load('C:\Users\MAE-NCSUCORE\Desktop\WaterChannelControl\output\data\data_18_Jun_2019_16_49_03.mat')
 
 %% Side
 % clc;
@@ -265,3 +266,121 @@ xlabel('x')
 ylabel('y')
 zlabel('z')
 
+%% Bottom Cam
+clc; clear;
+
+createSnellsMinParams;
+createRayTraceCtrlBus;
+createCoordinateCtrlBus;
+loadParams;
+load('C:\Users\MAE-NCSUCORE\Desktop\WaterChannelControl\output\data\data_18_Jun_2019_17_46_04.mat')
+timeIndex = 1000;
+
+camPosVec         = snells(7).camPosVec;
+% roll_deg          = tsc.roll_rad.data(end)*180/pi;
+% pitch_deg         = tsc.pitch_rad.data(end)*180/pi;
+% yaw_deg           = tsc.yaw_rad.data(end)*180/pi;
+camEulerAngles_deg = snells(7).camEulAng;
+glassThickness    = params.glassThickness_cm.Value;
+dist2Glass        = 3.4;
+glassPlane        = 1;
+indOfRef          = params.indexOfRefractionA.Value;
+imageDims_px      = params.sideImageSize_px.Value;
+cameraFOV_deg     = params.cameraFOV_deg.Value;
+pixCoords         = tsc.cam3DotPos.data(1:2,:,timeIndex);
+pixelRow          = pixCoords(1);
+pixelCol          = pixCoords(2);
+rayLength         = 50;
+camEulAngles_rad = camEulerAngles_deg*pi/180;
+grnd2CamRotMat = calculateRotationMatrix(....
+    camEulAngles_rad(1),camEulAngles_rad(2),camEulAngles_rad(3));
+
+sim('rayTrace_th')
+
+timeStart = 995;
+timeEnd = 1000;
+index = 1;
+
+len = timeEnd - timeStart;
+
+cam1 = zeros(len,6); 
+cam2 = zeros(len,6);
+cam3 = zeros(len,6);
+
+for ii = timeStart:timeEnd
+    cam1(index,:) = tsc.cam1DotPos.Data(:,:,ii);
+    cam2(index,:) = tsc.cam2DotPos.Data(:,:,ii);
+    cam3(index,:) = tsc.cam3DotPos.Data(:,:,ii);
+    
+    index = index + 1;
+end
+
+cam1Time = timeseries(cam1);
+cam2Time = timeseries(cam2);
+cam3Time = timeseries(cam3);
+
+sim('find6DOFSnellMin9_th')
+
+% camPosVec
+rogG_th = rogG.Data
+rogG_tsc = tsc.cam3Dot1OutVec.Data(:,:,timeIndex)'
+rogG_off = cam3Dot1OutVec.Data(:,:,len)'
+rigG_th = rigG.Data
+rigG_tsc = tsc.cam3Dot1InVec.Data(:,:,timeIndex)'
+rigG_off = cam3Dot1InVec.Data(:,:,len)'
+uiG_th = uiG.Data
+uiG_tsc = tsc.cam3Dot1UnitVec.Data(:,:,timeIndex)'
+uiG_off = cam3Dot1UnitVec.Data(:,:,len)'
+% rocC_th = rocC.Data
+% rocC_m = tsc.rocCSlant.Data(:,:,timeIndex)'
+% rioC_th = rioC.Data
+% rioC_m = tsc.rioCSlant.Data(:,:,timeIndex)'
+
+% dist2Glass
+% glassDist = norm(rogG_th - camPosVec)
+% 
+% gammaH_th = gammaH.Data
+% gammaH_m = tsc.gammaHSlant.Data(end)
+% 
+% gammaV_th = gammaV.Data
+% gammaV_m = tsc.gammaVSlant.Data(end)
+% 
+% figure(1)
+set(gca,'NextPlot','add')
+scatter3(...
+       rogG.Data(1),rogG.Data(2),rogG.Data(3),...
+       'Marker','o','CData',[0 1 0],'DisplayName','Ray Base');
+scatter3(...
+       rigG.Data(1),rigG.Data(2),rigG.Data(3),...
+       'Marker','o','CData',[0 0 1],'DisplayName','Ray Base');
+plot3(...
+   [rigG.Data(1) rigG.Data(1)+rayLength*uiG.Data(1)],...
+   [rigG.Data(2) rigG.Data(2)+rayLength*uiG.Data(2)],...
+   [rigG.Data(3) rigG.Data(3)+rayLength*uiG.Data(3)],...
+   'LineStyle','--','LineWidth',1,'Color','k','DisplayName','Ray Line');
+
+scatter3(...
+   camPosVec(1),camPosVec(2),camPosVec(3),...
+   'Marker','o','CData',[0 0 0],'DisplayName','Cam Pos');
+
+%
+
+scatter3(...
+       rogG_tsc(1),rogG_tsc(2),rogG_tsc(3),...
+       'Marker','*','CData',[0 1 0],'DisplayName','Ray Base');
+scatter3(...
+       rigG_tsc(1),rigG_tsc(2),rigG_tsc(3),...
+       'Marker','*','CData',[0 0 1],'DisplayName','Ray Base');
+plot3(...
+   [rigG_tsc(1) rigG_tsc(1)+rayLength*uiG_tsc(1)],...
+   [rigG_tsc(2) rigG_tsc(2)+rayLength*uiG_tsc(2)],...
+   [rigG_tsc(3) rigG_tsc(3)+rayLength*uiG_tsc(3)],...
+   'LineStyle',':','LineWidth',1,'Color','k','DisplayName','Ray Line');
+
+legend
+grid on
+axis equal
+axis square
+xlabel('x')
+ylabel('y')
+zlabel('z')
