1375f8841d5d47526a589c2d0d46738e5d82838d
jackdcole@gmail.com <Panthers24cole>
2019-06-27 17:58:46 -0400

    Changed models back to previous dot tracking - JC

--- units/imageUDPNode/imageUDPNode_init.m
+++ units/imageUDPNode/imageUDPNode_init.m
@@ -35,6 +35,7 @@ switch length(packingPorts)
 
     % Case with one input to Byte Packing block
     case 2
+        % If third port visible in block, delete port and signal line
         if ~isempty(block3)
             block3Ports = get_param(block3,'PortConnectivity');
             pos = block3Ports{1,1}.Position;
@@ -83,8 +84,28 @@ switch length(packingPorts)
             linePos = [posX+35, posY+10; posX+85, posY+10];
             bLine = add_line(gcb,linePos);
         end
+        
+    % Case with three inputs to Byte Packing block
     case 4
         % If second port not visible in block, add port and signal line
+        if isempty(block2)
+            add_block('simulink/Ports & Subsystems/In1',[gcb '/pixelCompB'])
+            
+            % Re-adjust position of pixelCompA port
+            posX = packingPorts(1).Position(1) - 85;
+            posY = packingPorts(1).Position(2) - 10;
+            pos = [posX, posY, posX+35, posY+15];
+            set_param([gcb '/pixelCompA'],'position',pos);
+            
+            % Re-adjust position of pixelCompB port and add signal line
+            posX = packingPorts(2).Position(1) - 85;
+            posY = packingPorts(2).Position(2) - 10;
+            pos = [posX, posY, posX+35, posY+15];
+            set_param([gcb '/pixelCompB'],'position',pos);
+            linePos = [posX+35, posY+10; posX+85, posY+10];
+            bLine = add_line(gcb,linePos);
+        end
+        % If third port not visible in block, add port and signal line
         if isempty(block3)
             add_block('simulink/Ports & Subsystems/In1',[gcb '/pixelCompC'])
             
