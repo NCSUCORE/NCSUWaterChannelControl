d549ceac8ef1d62f730aa6557a86a8bdc405bb24
jackdcole@gmail.com <Panthers24cole>
2019-07-15 19:06:42 -0400

    Modified angular optimization for current camera setup - JC

--- functions/createSnellsMinParams.m
+++ functions/createSnellsMinParams.m
@@ -5,38 +5,41 @@ function createSnellsMinParams()
 
 global params;
 
-% Camera 3
-snells(1).camPosVec = [4 -10 22.5]';
-snells(1).camEulAng = [3 179.4 0]';
-snells(1).camDist2Glass = params.botDistancetoGlass_cm.Value;
-snells(1).bodyFixedVec = [-1.134 -1.482 -4.554]';
+% Side Camera
+snells(1).camPosVec = params.sideCamPosVec_cm.Value';
+snells(1).camEulAng = params.sideCamEulAng_deg.Value';
+snells(1).camDist2Glass = params.sideDistancetoGlass_cm.Value;
+snells(1).bodyFixedVec = params.sideDotPosVec_cm.Value';
 
-snells(2) = snells(1);
-snells(2).bodyFixedVec = [-1.134 1.482 -4.554]';
+% snells(2) = snells(1);
+% snells(2).bodyFixedVec = [-1.134 1.482 -4.554]';
+% snells(3) = snells(2);
+% snells(3).bodyFixedVec = [2.049 0 -4.546]';
+
+% Bottom Camera
+snells(2).camPosVec = params.botCamPosVec_cm.Value';
+snells(2).camEulAng = params.botCamEulAng_deg.Value';
+snells(2).camDist2Glass = params.botDistancetoGlass_cm.Value;
+snells(2).bodyFixedVec = params.botADotPosVec_cm.Value';
+
 snells(3) = snells(2);
-snells(3).bodyFixedVec = [2.049 0 -4.546]';
+snells(3).bodyFixedVec = params.botBDotPosVec_cm.Value';
 
-% Camera 4
-snells(4).camPosVec = [0 0 23]';
-snells(4).camEulAng = [1.9 185 180]';
+% snells(5) = snells(4);
+% snells(5).bodyFixedVec = [-1.134 1.482 -4.554]';
+% snells(6) = snells(5);
+% snells(6).bodyFixedVec = [2.049 0 -4.546]';
+
+% Slant/Periscope Camera
+snells(4).camPosVec = params.prscpeCamPosVec_cm.Value';
+snells(4).camEulAng = params.prscpeCamEulAng_deg.Value';
 snells(4).camDist2Glass = params.botDistancetoGlass_cm.Value;
-snells(4).bodyFixedVec = [-1.134 -1.482 -4.554]';
+snells(4).bodyFixedVec = params.topDotPosVec_cm.Value';
 
-snells(5) = snells(4);
-snells(5).bodyFixedVec = [-1.134 1.482 -4.554]';
-snells(6) = snells(5);
-snells(6).bodyFixedVec = [2.049 0 -4.546]';
-
-% Camera 5
-snells(7).camPosVec = [-22.5 0 25.5]';
-snells(7).camEulAng = [1.5 128 182]';
-snells(7).camDist2Glass = params.botDistancetoGlass_cm.Value;
-snells(7).bodyFixedVec = [-1.134 -1.482 -4.554]';
-
-snells(8) = snells(7);
-snells(8).bodyFixedVec = [-1.134 1.482 -4.554]';
-snells(9) = snells(8);
-snells(9).bodyFixedVec = [2.049 0 -4.546]';
+% snells(8) = snells(7);
+% snells(8).bodyFixedVec = [-1.134 1.482 -4.554]';
+% snells(9) = snells(8);
+% snells(9).bodyFixedVec = [2.049 0 -4.546]';
 
 assignin('base','snells',snells)
 
--- units/snellLeastSquaresPosition/snellLeastSquaresPosition.m
+++ units/snellLeastSquaresPosition/snellLeastSquaresPosition.m
@@ -1,9 +1,11 @@
 function [COMPosVec,sideDotPos_cm,botADotPos_cm,botBDotPos_cm,topDotPos_cm]...
-    = snellLeastSquaresPosition(rCentroidSide,rCentroidBotA,...
-        rCentroidBotB,rCentroidSlant,uCentroidSide,uCentroidBotA,...
-        uCentroidBotB,uCentroidSlant,body2GroundRotMat,...
-        sideDotPosVec_cm,botADotPosVec_cm,botBDotPosVec_cm,topDotPosVec_cm)
+    = snellLeastSquaresPosition(rCentroid,uCentroid,body2GroundRotMat,bodyFixedVec)
 
+% (rCentroidSide,rCentroidBotA,...
+%         rCentroidBotB,rCentroidSlant,uCentroidSide,uCentroidBotA,...
+%         uCentroidBotB,uCentroidSlant,body2GroundRotMat,...
+%         sideDotPosVec_cm,botADotPosVec_cm,botBDotPosVec_cm,topDotPosVec_cm)
+    
 % This function calculates the center of mass of the body based on the
 % inputs from the ray trace algorithm. 
 
@@ -42,27 +44,73 @@ function [COMPosVec,sideDotPos_cm,botADotPos_cm,botBDotPos_cm,topDotPos_cm]...
 % botBDotPos_cm - position of the bottom B dot set in the ground frame
 % topDotPos_cm - position of the top dot set in the ground frame
 
-% Rotate dot set vectors into the ground frame
-sideDotGF = body2GroundRotMat*sideDotPosVec_cm(:);
-botADotGF = body2GroundRotMat*botADotPosVec_cm(:);
-botBDotGF = body2GroundRotMat*botBDotPosVec_cm(:);
-topDotGF = body2GroundRotMat*topDotPosVec_cm(:);
+% % Rotate dot set vectors into the ground frame
+% sideDotGF = body2GroundRotMat*sideDotPosVec_cm(:);
+% botADotGF = body2GroundRotMat*botADotPosVec_cm(:);
+% botBDotGF = body2GroundRotMat*botBDotPosVec_cm(:);
+% topDotGF = body2GroundRotMat*topDotPosVec_cm(:);
+% 
+% % Concatenate ray and dot set vectors vertically into 12x1 vector
+% rCentroidVec = [rCentroidSide(:); rCentroidBotA(:); rCentroidBotB(:); rCentroidSlant(:)];
+% COM2DotGF    = [sideDotGF; botADotGF; botBDotGF; topDotGF];
+% 
+% % Subtraction due to dot set vectors direction away from center of mass
+% P = rCentroidVec - COM2DotGF;
+% 
+% % Form block diagonal matrix populated by unit vectors toawrds dot sets
+% U = [...
+%     uCentroidSide(:) zeros(3,1)       zeros(3,1)       zeros(3,1) ;...
+%     zeros(3,1)       uCentroidBotA(:) zeros(3,1)       zeros(3,1) ;...
+%     zeros(3,1)       zeros(3,1)       uCentroidBotB(:) zeros(3,1) ;...
+%     zeros(3,1)       zeros(3,1)       zeros(3,1)       uCentroidSlant(:)];
+% 
+% % Form block diagonal martix which comprises the least squares subtraction
+% I = eye(3);
+% O = zeros(3);
+% Delta = [I -I  O  O;
+%          I  O -I  O;
+%          I  O  O -I;
+%          O  I -I  O;
+%          O  I  O -I;
+%          O  O  I -I];
+% 
+% % Perform the minimization to calculate the optimal distance from the ray
+% % exiting the glass to the dot set centroid
+% alpha = (Delta*U)'*(Delta*U);
+% d = -(alpha'*alpha)\(alpha'*U'*Delta'*Delta*P);
+% 
+% % Calculate the center of mass position based on the calculated distance
+% COMPosVec = U*d+P;
+% COMPosVec = reshape(COMPosVec,[3 4]); % Reshape to 3 row by 4 col
+% COMPosVec = mean(COMPosVec,2); % take mean over columns
+% 
+% % Calculate the position of the centroid of each dot set in the ground
+% % frame
+% sideDotPos_cm = COMPosVec + sideDotGF;
+% botADotPos_cm = COMPosVec + botADotGF;
+% botBDotPos_cm = COMPosVec + botBDotGF;
+% topDotPos_cm = COMPosVec + topDotGF;
 
-% Concatenate ray and dot set vectors vertically into 12x1 vector
-rCentroidVec = [rCentroidSide(:); rCentroidBotA(:); rCentroidBotB(:); rCentroidSlant(:)];
-COM2DotGF    = [sideDotGF; botADotGF; botBDotGF; topDotGF];
+%% Least squares formulation for Powell's method optimization algorithm
 
-% Subtraction due to dot set vectors direction away from center of mass
-P = rCentroidVec - COM2DotGF;
+% (rCentroid,uCentroid,body2GroundRotMat,bodyFixedVec)
 
-% Form block diagonal matrix populated by unit vectors toawrds dot sets
+dotVecGF = body2GroundRotMat*bodyFixedVec;
+
+rCentroidVec = [rCentroid(:,1);rCentroid(:,2);rCentroid(:,3);...
+                rCentroid(:,4)];
+
+dotVecGFVec = [dotVecGF(:,1);dotVecGF(:,2);dotVecGF(:,3);...
+               dotVecGF(:,4)];
+
+P = rCentroidVec - dotVecGFVec;
+
 U = [...
-    uCentroidSide(:) zeros(3,1)       zeros(3,1)       zeros(3,1) ;...
-    zeros(3,1)       uCentroidBotA(:) zeros(3,1)       zeros(3,1) ;...
-    zeros(3,1)       zeros(3,1)       uCentroidBotB(:) zeros(3,1) ;...
-    zeros(3,1)       zeros(3,1)       zeros(3,1)       uCentroidSlant(:)];
+    uCentroid(:,1)  zeros(3,1)      zeros(3,1)      zeros(3,1);...
+    zeros(3,1)      uCentroid(:,2)  zeros(3,1)      zeros(3,1);...
+    zeros(3,1)      zeros(3,1)      uCentroid(:,3)  zeros(3,1);...
+    zeros(3,1)      zeros(3,1)      zeros(3,1)      uCentroid(:,4)]; 
 
-% Form block diagonal martix which comprises the least squares subtraction
 I = eye(3);
 O = zeros(3);
 Delta = [I -I  O  O;
@@ -72,61 +120,6 @@ Delta = [I -I  O  O;
          O  I  O -I;
          O  O  I -I];
 
-% Perform the minimization to calculate the optimal distance from the ray
-% exiting the glass to the dot set centroid
-alpha = (Delta*U)'*(Delta*U);
-d = -(alpha'*alpha)\(alpha'*U'*Delta'*Delta*P);
-
-% Calculate the center of mass position based on the calculated distance
-COMPosVec = U*d+P;
-COMPosVec = reshape(COMPosVec,[3 4]); % Reshape to 3 row by 4 col
-COMPosVec = mean(COMPosVec,2); % take mean over columns
-
-% Calculate the position of the centroid of each dot set in the ground
-% frame
-sideDotPos_cm = COMPosVec + sideDotGF;
-botADotPos_cm = COMPosVec + botADotGF;
-botBDotPos_cm = COMPosVec + botBDotGF;
-topDotPos_cm = COMPosVec + topDotGF;
-
-%% Least squares formulation for optimization algorithm
-
-% (rCentroid,uCentroid,body2GroundRotMat,bodyFixedVec)
-
-% dotVecGF = body2GroundRotMat*bodyFixedVec;
-% 
-% rCentroidVec = [rCentroid(:,1);rCentroid(:,2);rCentroid(:,3);...
-%                 rCentroid(:,4);rCentroid(:,5);rCentroid(:,6);...
-%                 rCentroid(:,7);rCentroid(:,8);rCentroid(:,9)];
-
-% rCentroidVec = [rCentroid(:,1);rCentroid(:,2);rCentroid(:,3)];
-
-% dotVecGFVec = [dotVecGF(:,1);dotVecGF(:,2);dotVecGF(:,3);...
-%                dotVecGF(:,4);dotVecGF(:,5);dotVecGF(:,6);...
-%                dotVecGF(:,7);dotVecGF(:,8);dotVecGF(:,9)];
-
-% dotVecGFVec = [dotVecGF(:,1);dotVecGF(:,2);dotVecGF(:,3)];
-
-% P = rCentroidVec - dotVecGFVec;
-% 
-% U = [...
-%     uCentroid(:,1)  zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1);...
-%     zeros(3,1)      uCentroid(:,2)  zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1);...
-%     zeros(3,1)      zeros(3,1)      uCentroid(:,3)  zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1);...
-%     zeros(3,1)      zeros(3,1)      zeros(3,1)      uCentroid(:,4)  zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1);...
-%     zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      uCentroid(:,5)  zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1);...
-%     zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      uCentroid(:,6)  zeros(3,1)      zeros(3,1)      zeros(3,1);...
-%     zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      uCentroid(:,7)  zeros(3,1)      zeros(3,1);...
-%     zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      uCentroid(:,8)  zeros(3,1);...
-%     zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      zeros(3,1)      uCentroid(:,9)]; 
-
-% U = [...
-%      uCentroid(:,1)  zeros(3,1)      zeros(3,1);...
-%      zeros(3,1)      uCentroid(:,2)  zeros(3,1);...
-%      zeros(3,1)      zeros(3,1)      uCentroid(:,3)];
-
-% I = eye(3);
-% O = zeros(3);
 % Delta = [I -I  O  O  O  O  O  O  O;...
 %          I  O -I  O  O  O  O  O  O;...
 %          I  O  O -I  O  O  O  O  O;...
@@ -164,17 +157,18 @@ topDotPos_cm = COMPosVec + topDotGF;
 %          O  O  O  O  O  O  I  O -I;...
 %          O  O  O  O  O  O  O  I -I];
 
-% Delta = [I -I  O;
-%          I  O -I;
-%          O  I -I];
+alpha = (Delta*U)'*(Delta*U);
 
-% alpha = (Delta*U)'*(Delta*U);
-% 
-% d = -(alpha'*alpha)\(alpha'*U'*Delta'*Delta*P);
-% 
-% COMPosVec = U*d+P;
-% COMPosVec = reshape(COMPosVec,[3 9]); % Reshape to 3 row by 9 col
-% COMPosVec = mean(COMPosVec,2); % take mean over columns
+d = -(alpha'*alpha)\(alpha'*U'*Delta'*Delta*P);
+
+COMPosVec = U*d+P;
+COMPosVec = reshape(COMPosVec,[3 4]); % Reshape to 3 row by 9 col
+COMPosVec = mean(COMPosVec,2); % take mean over columns
+
+sideDotPos_cm = COMPosVec + dotVecGFVec(1:3);
+botADotPos_cm = COMPosVec + dotVecGFVec(4:6);
+botBDotPos_cm = COMPosVec + dotVecGFVec(7:9);
+topDotPos_cm = COMPosVec + dotVecGFVec(10:12);
 
 end
 
